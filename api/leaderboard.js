a614ba0ce83fc1282abf10f0c214b6242d636b45
arukh89 <85455057+arukh89@users.noreply.github.com>
2025-09-10 15:03:00 +0700

    Update leaderboard.js

--- api/leaderboard.js
+++ api/leaderboard.js
@@ -1,6 +1,2 @@
 export const guesses = new Map();
-export function setRealTxCount(blockHeight, realTxCount) {
-  for (const [k, g] of guesses.entries()) {
-    if (g.blockHeight === blockHeight) g.diff = Math.abs(g.guess - realTxCount);
-  }
-}
+export const tgtBlock = 913889; // Ensure this matches the target block in your index.html
