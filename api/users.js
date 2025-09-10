8780330a9a7c546c26233b96c56ac62e23fe0a64
arukh89 <85455057+arukh89@users.noreply.github.com>
2025-09-10 09:17:24 +0700

    Update users.js

--- api/users.js
+++ api/users.js
@@ -5,10 +5,12 @@ export default async (req, res) => {
     try {
       // Use the Neynar API to fetch users
       const neynarApiKey = 'DIA557C9-F55E-42F3-8987-0F2CA80DD4E';
+      const neynarClientId = '30719794-4961-4a3a-9aeb-c0e33086a64e';
       const response = await fetch('https://snapchain-api.neynar.com/v1/users', {
         headers: {
           'Content-Type': 'application/json',
           'x-api-key': neynarApiKey,
+          'x-client-id': neynarClientId,
         },
       });
       const data = await response.json();
